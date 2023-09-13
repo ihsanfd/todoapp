be2ebb40dcbc19c3e22da37fe27877adf1f16de4
Ihsan F. Dokumacioglu <ihsan.furkan@gmail.com>
2023-09-13 18:38:26 +0300

    Proje Bitti

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Model } from '../model';
+import { TodoItem } from '../todoitem';
 
 @Component({
   selector: 'app-todo',
@@ -9,18 +10,51 @@ import { Model } from '../model';
 export class TodoComponent {
 
   displayAll: boolean = false;
+  inputText: string = "";
   constructor() { }
 
   message = "";
   
   model = new Model();
 
-  addItem(value: string) {
-    if (value != "") {
-    this.model.items.push({ description: value, action: false });
+  addItem() {
+    if (this.inputText!= "") {
+   let data = {description: this.inputText, action: false};
+   this.model.items.push(data);
+
+   let items= this.getItemsFromLS();
+   this.model.items.push(data);
+   localStorage.setItem('items', JSON.stringify(items));
+   this.inputText = "";
     } else {
       alert("Değer giriniz.");
     }
+  }
+
+  getItemsFromLS() {
+    let items : TodoItem[] = [];
+    let value= localStorage.getItem('items');
+    if (value != null) {
+      let values = localStorage.getItem('items');
+
+      if (values != null) {
+        items = JSON.parse(value);
+      }
+    }
+    return items;
+  }
+
+  onActionChanged(item: TodoItem) {
+    let items = this.getItemsFromLS();
+    localStorage.clear();
+
+    items.forEach(i => {
+      if (i.description == item.description) {
+        i.action = i.action;
+      }
+    });
+
+    localStorage.setItem('items', JSON.stringify(items));
   }
 
   
@@ -34,5 +68,21 @@ export class TodoComponent {
     }
     return this.model.items.filter(item => !item.action);
   }
+  displayCount() {
+    return this.model.items.filter(i => i.action).length;
+  }
+
+  remainingCount() {
+    return this.model.items.filter(i => !i.action).length;
+  }
+  getBtnClasses(){
+    return {
+    'disabled': this.inputText.length == 0,
+   ' btn-secondary': this.inputText.length == 0,
+    'btn-primary': this.inputText.length > 0
+    }
+
+  }
 
 }
+
